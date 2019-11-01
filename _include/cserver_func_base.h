ba0b4061ef44221f24843f6ac79c8f92fee5cebe
Q1143316492 <1143316492@qq.com>
2019-11-01 15:42:52 +0800

    test

--- _include/cserver_func_base.h
+++ _include/cserver_func_base.h
@@ -1,5 +1,11 @@
 #ifndef _CSERVER_FUNC_BASE_H_
 #define _CSERVER_FUNC_BASE_H_
 
+#include "cserver_macro.h"
+// logs
+
+void print_log(const char*file, const char* func, int line, const char* level, const char* fmt, ...);
+
+void write_log_to_file(const char*file, const char* func, int line, const char* level, const char* buf);
 
 #endif
